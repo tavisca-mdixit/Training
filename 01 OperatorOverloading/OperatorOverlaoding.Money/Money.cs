260d5b300f6587b2cc15fede01443fa6bae6b7b3
unknown <mdixit@TavWITPD041.internal.tavisca.com>
2015-07-07 12:51:31 +0530

    Pushing OperatorOverloading

---
+++ 01 OperatorOverloading/OperatorOverlaoding.Money/Money.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloadingMoney
+    {
+    public class Money
+    {
+
+        public double Amount { get; set; }
+        public string Currency { get; set; }
+
+        public static Money operator+(Money moneyOne,Money moneyTwo)  {
+            Money moneyThree = new Money();
+          
+                if (moneyOne.Currency.Equals(moneyTwo.Currency))
+                {
+                    
+                    moneyThree.Amount= moneyOne.Amount + moneyTwo.Amount;
+                    if (double.IsInfinity(moneyThree.Amount) || moneyThree.Amount<0)
+                    { throw new System.ArgumentException(); }
+                    moneyThree.Currency = moneyOne.Currency;
+                    return moneyThree;
+                }
+                else{
+                throw new System.Exception();}
+
+        
+        }
+        
+    }
+}
---
+++ 01 OperatorOverloading/OperatorOverloading.Host/Host.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OperatorOverloadingMoney;
+
+namespace OperatorOverloading.Host
+{
+    class Host
+    {
+        static void Main(string[] args)
+        {
+            Money Money1 = new Money();
+            Money Money2 = new Money();
+            Money Money3 = new Money();
+
+            Console.WriteLine("Enter the Curreny type  USD/INR and the amount ");
+            Money1.Currency = Console.ReadLine();
+          Money1.Amount = Convert.ToDouble(Console.ReadLine());
+   
+
+            Console.WriteLine("Enter the Curreny type  USD/INR and the amount ");
+            Money2.Currency = Console.ReadLine();
+           Money2.Amount = Convert.ToDouble(Console.ReadLine());
+        
+
+            try
+            {
+                Money3 = Money2 + Money1;
+            }
+            catch(Exception E){
+                Console.WriteLine("Exception Caught");
+                Console.WriteLine("Cuurent /Amount Invalid");
+                   Console.WriteLine(E.StackTrace);
+
+            }
+
+            Console.WriteLine("The Currency and Amount is : {0}  {1}", Money3.Currency, Money3.Amount);
+
+          
+            Console.ReadKey();
+        }
+    }
+}
