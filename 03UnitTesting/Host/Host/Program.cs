3b78f954a5cc1e24c3cef4bb4fc203078efd6a5c
tavisca-mdixit <mdixit@tavisca.com>
2015-07-17 10:01:42 +0530

    Commiting UnitTesting after making chnages accroding to the review

--- 03UnitTesting/Host/Host/Program.cs
+++ 03UnitTesting/Host/Host/Program.cs
@@ -13,37 +13,45 @@ namespace Host
         public static void Main(string[] args)
         {
             Console.WriteLine("Enter the Category type:");
-            var category = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(args[0]) || string.IsNullOrWhiteSpace(args[1]))
+            {
+                Console.WriteLine("Invalid Arguments entered");
+                Console.ReadKey();
+                return;
+
+            }
+            var category = args[1];
+
+
             try
             {
-                if (string.IsNullOrWhiteSpace(args[0]) || string.IsNullOrWhiteSpace(category))
-                {
-                    throw new ArgumentException("Invalid Input Entered");
-                }
-            }
-            catch (ArgumentException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            Assembly assembly = Assembly.LoadFrom(args[0]);
+                Assembly assembly = Assembly.LoadFrom(args[0]);
 
-            foreach (Type type in assembly.GetTypes())
-            {
-                if (type.IsClass && TestClassAttribute.Exists(type))
+
+                foreach (Type type in assembly.GetTypes())
                 {
-                    Console.WriteLine("The name of class is :{0}\n", type.FullName);
-                    foreach (MethodInfo method in (type.GetMethods()))
+                    if (type.IsClass && TestClassAttribute.Exists(type))
                     {
-                        
-                        if (TestMethodAttribute.Exists(method))
+                        Console.WriteLine("The name of class is :{0}\n", type.FullName);
+                        foreach (MethodInfo method in (type.GetMethods()))
                         {
-                            if (IgnoreAttribute.Exists(method))
-                                Console.WriteLine("The name of ignored  method is :{0}", method.Name);
-                            else if (string.IsNullOrWhiteSpace(category) || TestCategoryAttribute.Exists(method, category))
-                                Console.WriteLine("The name of executable  method is :{0}\n", method.Name);
+
+                            if (TestMethodAttribute.Exists(method))
+                            {
+                                if (string.IsNullOrWhiteSpace(category) || TestCategoryAttribute.Exists(method, category))
+                                    Console.WriteLine("The name of executable  method is :{0}\n", method.Name);
+                                else if (IgnoreAttribute.Exists(method))
+                                    Console.WriteLine("The name of ignored  method is :{0}", method.Name);
+
+                            }
                         }
                     }
                 }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
             }
             Console.ReadKey();
         }
