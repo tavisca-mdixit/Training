c2f4c7e186aa3c5437f2c1ed6902cd2e8cdd653f
tavisca-mdixit <mdixit@tavisca.com>
2015-07-20 00:09:06 +0530

    Bouncing Ball

---
+++ 05BouncingBall/Script/StringManipulation.js
@@ -0,0 +1,39 @@
+﻿
+var previousX = 0;
+var previousY = 0;
+var dX = 20;
+var dY = 20;
+
+function startBouncing() {
+    setInterval(rollBall, 20);
+
+}
+function rollBall() {
+    var height = (window.innerHeight - 50);
+    var width = (window.innerWidth - 50);
+    previousX += dX;
+    previousY += dY;
+    var element = document.getElementById('Ball');
+    element.style.background = "cyan";
+    element.style.left = previousX + "px";
+    element.style.top = previousY + "px";
+
+
+    if (previousY >= height) {
+        dY = -10;
+        return;
+    }
+    if (previousX >= width) {
+        dX = -10;
+        return;
+    }
+    if (previousY <= 0) {
+        dY = 10;
+        return;
+    }
+    if (previousX <= 0) {
+        dX = 10;
+        return;
+    }
+
+}
