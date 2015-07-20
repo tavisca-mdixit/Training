9518aadc1479c84b262bb223d674fbd615089668
tavisca-mdixit <mdixit@tavisca.com>
2015-07-20 22:25:07 +0530

    Made changes according to changes

---
+++ 05BouncingBall/Script/ContainerView.js
@@ -0,0 +1,23 @@
+﻿window.ball = window.ball || {
+
+};
+
+var increment=10;
+window.ball.boundaryCheck = function () {
+
+    ball.height = (window.innerHeight - 50);
+    ball.width = (window.innerWidth - 50);
+
+    if (ball.previousY >= ball.height) {
+        ball.gradientY = -increment;
+    }
+    if (ball.previousX >= ball.width) {
+        ball.gradientX = -increment;
+    }
+    if (ball.previousY <= 0) {
+        ball.gradientY = increment;
+    }
+    if (ball.previousX <= 0) {
+        ball.gradientX = increment;
+    }
+}
